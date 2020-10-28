2f83e52fb5f50fd5344d885d2ea6bcad47bf859d
Ihor Levchenko <ihor224@gmail.com>
2020-10-28 17:47:40 +0200

    Добавляет разметку, стили и тему

---
+++ src/index.js
@@ -0,0 +1,59 @@
+import menuCardsTpl from "./templates/cards.hbs";
+import menu from "./js/menu.json";
+import "./css/styles.css";
+
+const cardsMarkup = createMenuCardsMarkup(menu);
+const container = document.querySelector(".js-menu");
+
+container.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createMenuCardsMarkup(menu) {
+  return menu.map(menuCardsTpl).join("");
+}
+
+const Theme = {
+  LIGHT: "light-theme",
+  DARK: "dark-theme",
+};
+
+const body = document.querySelector("body");
+const switchTheme = document.querySelector(".theme-switch__toggle");
+
+switchTheme.addEventListener("change", clickOnSwitch);
+switchTheme.addEventListener("change", setLocalStorage);
+document.addEventListener("DOMContentLoaded", themeFromLocalStorage);
+
+function clickOnSwitch() {
+  if (switchTheme.checked) {
+    setDarkTheme();
+  } else {
+    setLightTheme();
+  }
+}
+
+function setDarkTheme() {
+  body.classList.add(Theme.DARK);
+  body.classList.remove(Theme.LIGHT);
+}
+
+function setLightTheme() {
+  body.classList.add(Theme.LIGHT);
+  body.classList.remove(Theme.DARK);
+}
+
+function setLocalStorage() {
+  if (switchTheme.checked) {
+    localStorage.setItem("theme", Theme.DARK);
+  } else {
+    localStorage.removeItem("theme");
+    localStorage.setItem("theme", Theme.LIGHT);
+  }
+}
+
+function themeFromLocalStorage() {
+  const themeInLocalStorage = localStorage.getItem("theme");
+  if (themeInLocalStorage === Theme.DARK) {
+    body.classList.add(Theme.DARK);
+    switchTheme.checked = true;
+  }
+}
